7d3d3c692a63add2710f6c0e32b2f5d0820c914b
Sanjay-SG <sanjay.s.gunagi@gmail.com>
2021-10-24 02:28:42 +0530

    commit 2

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import {main} from './index.js';
+import fs from 'fs';
+
 
 const app = express();
 
@@ -9,8 +11,43 @@ const PORT = 5000;
 
 app.get('/', async (req, res) => {
     let result = await main();
-    res.send(result);
-    res.send('logged in ');
+    await res.send(result);
+    // await res.send('logged in ');
+
+
+/*     await fs.writeFile('./myfile.txt', 'Content to write', { flag: 'w' }, function(err) {
+        if (err) 
+            return console.error(err); 
+        fs.readFile('./myfile.txt', 'utf-8', function (err, data) {
+            if (err)
+                return console.error(err);
+            console.log(data);
+        });
+    });
+ */
+
+    const content = 'Some content!'
+    
+    fs.writeFile('test1.txt', JSON.stringify(result), err => {
+      if (err) {
+        console.error(err)
+        return
+      }
+      //file written successfully
+    })
+    
+
+
+/*     const content = 'Some content!'
+
+    try {
+      const data = fs.writeFileSync('D:/Workspace/Projects/test1/test1.txt', content)
+      //file written successfully
+    } catch (err) {
+      console.error(err)
+    }
+     */
+
 });
 
 app.listen(PORT, () => {
